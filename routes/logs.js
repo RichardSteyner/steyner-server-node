ae3b852adaeed9a6f80bd9c5ceffd304be20304c
Steyner <steyner.urupeque.s@gmail.com>
2022-08-18 20:32:14 -0500

    por ahora será publico la creación y actualización de log

--- routes/logs.js
+++ routes/logs.js
@@ -26,9 +26,9 @@ router.get('/:id',[
     validarCampos,
 ], obtenerLog );
 
-// Crear log - privado - cualquier persona con un token válido
+// Crear log - privado - cualquier persona con un token válido (por ahora público)
 router.post('/', [ 
-    validarJWT,
+    //validarJWT,
     check('titulo','El titulo es obligatorio').not().isEmpty(),
     check('tipo','El tipo es obligatorio').not().isEmpty(),
     check('detalleTipo','El detalle tipo es obligatorio').not().isEmpty(),
@@ -37,9 +37,9 @@ router.post('/', [
     validarCampos
 ], crearLog );
 
-// Actualizar log - privado - cualquiera con token válido
+// Actualizar log - privado - cualquiera con token válido (por ahora público)
 router.put('/:id',[
-    validarJWT,
+    //validarJWT,
     check('titulo','El titulo es obligatorio').not().isEmpty(),
     check('tipo','El tipo es obligatorio').not().isEmpty(),
     check('detalleTipo','El detalle tipo es obligatorio').not().isEmpty(),
