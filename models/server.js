6c5b8369021cf80d29fdc3b8b058175867701b54
RICHARD <steyner.urupeque.s@gmail.com>
2023-06-21 18:42:39 -0500

    Add cors localhost (nginx)

--- models/server.js
+++ models/server.js
@@ -40,7 +40,7 @@ class Server {
 
         // CORS
         this.app.use( cors( {
-			origin: ['http://localhost:3001', 'https://log-api-lwc.herokuapp.com', 'http://log-api-lwc.herokuapp.com', 'http://localhost:4200', 'http://log-app-build-production.up.railway.app', 'https://log-app-build-production.up.railway.app']
+			origin: ['http://localhost', 'https://log-api-lwc.herokuapp.com', 'http://log-api-lwc.herokuapp.com', 'http://localhost:4200', 'http://log-app-build-production.up.railway.app', 'https://log-app-build-production.up.railway.app']
 		} ) );
 
         // Lectura y parseo del body
