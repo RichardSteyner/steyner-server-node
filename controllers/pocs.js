ba0428a8eba8924b655b2ca4e8fc90e3193bbf7c
RICHARD <steyner.urupeque.s@gmail.com>
2023-12-16 18:34:18 -0500

    remove node-record-lpcm16 and debug packages

--- controllers/pocs.js
+++ controllers/pocs.js
@@ -1,55 +1,7 @@
 const { response, request } = require('express');
 const say = require('say');
-const recorder = require('node-record-lpcm16');
 
 const fs = require('fs');
-const path = require('path');
-
-const pocsGetWithRecording = async(req = request, res = response) => {
-    try{
-
-        const msg = 'OK'
-        const fileName = 'audio.wav';
-        const { text = '' } = req.query;
-
-        // Redirigir la salida de la consola a un archivo
-        const file = fs.createWriteStream(path.join(__dirname, fileName), { encoding: 'binary' });
-        const recording = recorder.record(
-            {
-                recorder: 'sox'
-            }
-        );
-        recording.stream()
-            .pipe(file);
-
-        console.log('Empieza a grabar')
-
-        //Reproducir el sonido
-        //say.speak(text);
-        say.speak(text, null, 1.0, (err) => {
-            if (err) {
-                console.error('Error en speak', err);
-                return res.status(500).json(err);
-            }
-            console.log('Text has been spoken.');
-            setTimeout(()=> {
-                recording.stop();
-                console.log('Recording STOP.');
-                file.end();
-                console.log('FILE END.');
-            }, 2000)
-            
-          }
-        );
-        console.log('res');
-        res.json({
-            msg
-        });
-    } catch(err) {
-        console.error('Error en catch', err);
-        res.status(500).json(err);
-    }
-}
 
 const pocsGet = async(req = request, res = response) => {
     try{
